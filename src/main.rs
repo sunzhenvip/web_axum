ec83341db796dd3e2ef7945bcdd3e176aa0ca601
sunzhen <1293334778@qq.com>
2025-05-22 22:45:44 +0800

    x1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use axum_weibo::run;
+#![allow(unused_imports)] // 作用于整个文件 消除没有使用的导入警告
+#![allow(dead_code)] // 消除 未使用的类型/函数/枚举变体 警告
+#![allow(unused_variables)] // 消除 未使用的变量/参数
+
+use axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
-    run().await;
+    let app = Router::new().route("/home", get(|| async { "Hello, world!" }));
 }
